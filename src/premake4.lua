002bdcf784ebb035c90f03e9de17e4aa0792578f
Jeff Davidson <jpd@google.com>
2016-12-25 20:52:40 -0800

    Fix the Linux build. (#40)
    This change brings up functional local and continuous Linux builds.
    XWord runs and generally functions (after some manual tweaks to
    the output directory structure as noted below), though more work
    will be needed to smooth rough edges and fix Linux-specific bugs.
    
    Changes of interest:
    
    -Added Linux prebuilts of premake5 and LuaJIT 2.0.4
    -Fix incorrect case on many imports
    -Fix missing imports for various C functions
    -Fix link order for lua builds
    -Add Travis CI build for Linux
    
    Still pending investigation:
    
    -libpuz.so is linked incorrectly (it depends on
    ../../bin/Debug/libyajl.so, but should just depend on libyajl.so).
    Worked around by linking libyajl.so from XWord in addition.
    -Output directories need manual tweaking:
     * Link images/ to share/XWord/images
     * Link scripts/ to lib/XWord/scripts
     * Move bin/Debug/scripts/libs/libX.so to lib/XWord/X.so
     * bin/Debug/libluapuz.so also expected at lib/XWord/luapuz.so
    
    Additional bugs:
    
    -Alignment of lua errors in status bar is wrong
    -Package manager encounters assertion errors
    -Clue bar increases in size every startup

--- src/premake4.lua
+++ src/premake4.lua
@@ -18,12 +18,7 @@ project "XWord"
     configuration "not macosx"
         excludes { "dialogs/wxFB_PreferencesPanelsOSX.*" }
 
-    -- --------------------------------------------------------------------
-    -- wxWidgets
-    -- --------------------------------------------------------------------
-
-    dofile "../premake4_wxdefs.lua"
-    dofile "../premake4_wxlibs.lua"
+    configuration { }
 
     -- --------------------------------------------------------------------
     -- puz
@@ -37,8 +32,13 @@ project "XWord"
         }
 
     configuration "linux"
-        defines { [[PUZ_API=""]] }
-        links { "dl" }
+        defines {
+            "PUZ_API=",
+            "LUAPUZ_API=",
+        }
+        -- TODO: Why is yajl necessary, when it's only needed by puz?
+        -- libpuz.so seems to link to libyajl.so via a bad relative path
+        links { "dl", "yajl" }
 
     configuration "macosx"
         defines {
@@ -96,7 +96,7 @@ project "XWord"
                 "-lwxbindcore",
                 "-lwxbindbase",
                 "-lwxlua",
-                "-llua5.1",
+                "-llua51",
             }
 
         -- Postbuild: copy lua51.dll to XWord directory
@@ -114,6 +114,15 @@ project "XWord"
                 "xwordlua*",
             }
     end
+
+    -- --------------------------------------------------------------------
+    -- wxWidgets
+    -- --------------------------------------------------------------------
+
+    -- Note: This must come after the link options above to ensure correct
+    -- link order on Linux.
+    dofile "../premake4_wxdefs.lua"
+    dofile "../premake4_wxlibs.lua"
 
     -- --------------------------------------------------------------------
     -- Resources
