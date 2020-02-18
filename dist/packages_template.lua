97773995b065d600f839e1dfa1599c4688cf0dd3
Jeff Davidson <jpd@google.com>
2020-02-17 16:46:09 -0800

    Point jpd236 fork to its own releases for auto update testing.

--- dist/packages_template.lua
+++ dist/packages_template.lua
@@ -1,6 +1,6 @@
 return {
   xword = {
-    download = "https://github.com/mrichards42/xword/releases/download/{VERSION}/{OS_FILE}",
+    download = "https://github.com/jpd236/xword/releases/download/{VERSION}/{OS_FILE}",
     name = "XWord",
     version = "{VERSION}",
   },
